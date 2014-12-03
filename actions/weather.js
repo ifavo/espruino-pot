d85328bbd20e2a7d7b7bcbe2085028da1be1f809
Mario Micklisch <mario@favo.org>
2014-12-03 20:08:28 +0100

    weather status page

--- actions/weather.js
+++ actions/weather.js
@@ -38,3 +38,43 @@ exports.weatherTimeframe = {
 		}
   }
 };
+
+exports.weatherStatus = {
+  name: 'weather/status',
+  description: 'gets the latest weather status',
+  blockedConnectionTypes: [],
+  outputExample: {},
+  matchExtensionMimeType: false,
+  version: 1.0,
+  toDocument: true,
+
+  inputs: {
+    required: [],
+    optional: []
+  },
+  run: function(api, connection, next) {
+		api.models.Weather
+			.findOne({limit: 1, order: [['time', 'DESC']]})
+			.then(function(raw) {
+				if (!raw) {
+					getError("no data found");
+					connection.rawConnection.responseHttpCode = 404;
+					return next(connection, true);
+				}
+				return raw;
+		})
+		.then(getSuccess, getError)
+		.finally(function() {
+			next(connection, true);
+		});
+
+		function getSuccess(raw) {
+			connection.response.weather = raw;
+		}
+
+		function getError(err) {
+			api.log('Could not get weather data', 'error');
+			connection.error = err;
+		}
+  }
+};
